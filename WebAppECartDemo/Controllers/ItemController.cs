cd9c6682f1b9b645a52f7e6310e2b8aee9d6adf6
Sefa Yılmaz <sefayilmaz582@gmail.com>
2022-01-01 22:42:28 +0300

    Add project files.

---
+++ WebAppECartDemo/Controllers/ItemController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppECartDemo.Models;
+using WebAppECartDemo.ViewModel;
+
+namespace WebAppECartDemo.Controllers
+{
+    public class ItemController : Controller
+    {
+
+        private ECartDBEntities objECartDBEntities;
+        public ItemController()
+        {
+            objECartDBEntities = new ECartDBEntities();
+        }
+
+        // GET: Item
+        public ActionResult Index()
+        {
+            ItemViewModel objItemViewModel = new ItemViewModel();
+            objItemViewModel.CategorySelectListItem = (from objCat in objECartDBEntities.Categories
+                select new SelectListItem()
+                {
+                     Text = objCat.CategoryName,
+                     Value = objCat.CategoryId.ToString(),
+                     Selected = true
+                 });
+
+
+
+            return View(objItemViewModel);
+        }
+        [HttpPost]
+        public JsonResult Index(ItemViewModel objItemViewModel)
+        {
+            string NewImage = Guid.NewGuid() + Path.GetExtension(objItemViewModel.ImagePath.FileName);
+            objItemViewModel.ImagePath.SaveAs(Server.MapPath("~/Images/" + NewImage));
+
+            Item objItem = new Item();
+            objItem.ImagePath = "~/Images/" + NewImage;
+            objItem.CategoryId = objItemViewModel.CategoryId;
+            objItem.Description = objItemViewModel.Description;
+            objItem.ItemCode = objItemViewModel.ItemCode;
+            objItem.ItemId = Guid.NewGuid();
+            objItem.ItemName = objItemViewModel.ItemName;
+            objItem.ItemPrice = objItemViewModel.ItemPrice;
+            objECartDBEntities.Items.Add(objItem);
+            objECartDBEntities.SaveChanges();
+
+
+
+            return Json(new {Success = true, Message = "Item is added Successfully." }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ WebAppECartDemo/Controllers/ShoppingController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppECartDemo.Models;
+using WebAppECartDemo.ViewModel;
+
+namespace WebAppECartDemo.Controllers
+{
+    public class ShoppingController : Controller
+    {
+        private ECartDBEntities objECartDbEntities;
+
+        public ShoppingController()
+        {
+            objECartDbEntities = new ECartDBEntities();
+        }
+        // GET: Shopping
+        public ActionResult Index()
+        {
+            IEnumerable<ShoppingViewModel> listOfShoppingViewModels = (from objItem in objECartDbEntities.Items
+                 join
+                 objCate in objECartDbEntities.Categories
+                 on objItem.CategoryId equals objCate.CategoryId
+                 select new ShoppingViewModel()
+                 {
+                     ImagePath = objItem.ImagePath,
+                     ItemName = objItem.ItemName,
+                     Description = objItem.Description,
+                     ItemPrice = objItem.ItemPrice,
+                     ItemId = objItem.ItemId,
+                     Category = objCate.CategoryName
+
+                 }
+                ).ToList();
+
+            return View(listOfShoppingViewModels);
+        }
+    }
+}
