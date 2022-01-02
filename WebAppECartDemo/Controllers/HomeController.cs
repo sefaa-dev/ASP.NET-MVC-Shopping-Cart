f308f26295e849557fae89512a217e094e5b16ec
Sefa Yılmaz <sefayilmaz582@gmail.com>
2022-01-02 20:23:50 +0300

    sefa

--- WebAppECartDemo/Controllers/HomeController.cs
+++ WebAppECartDemo/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace WebAppECartDemo.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : Controller 
     {
         // GET: Home
         public ActionResult Index()
